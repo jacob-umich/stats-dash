6e2f7cec72e3b1e39e58b8e25c42503be3238512
Jacob <jpavelka@umich.edu>
2024-04-05 14:40:04 -0400

    explored data to find augmenting data

--- format_data.py
+++ format_data.py
@@ -1,3 +1,14 @@
 import pandas as pd
 
-pd.read_csv
+data = pd.read_csv("Chronic_Disease_Indicators.csv")
+
+print(len(data.columns))
+
+# print(data[data["Response"].notna()][["Question","Response"]])
+with open("ques.md","wt") as f:
+    data.groupby("Question").size().sort_values(ascending=False).reset_index().to_markdown(buf=f)
+
+
+# print(data[data["Question"]=="Food insecure in the past 12 months among households"][["YearStart","YearEnd","LocationAbbr","DataValueUnit","DataValueType","DataValue"]].sort_values("LocationAbbr").to_markdown())
+
+# print(data[data["Question"]=="Food insecure in the past 12 months among households"][["LowConfidenceLimit","Stratification1","StratificationCategory1","Geolocation","LocationID","DataValue"]].to_markdown())
